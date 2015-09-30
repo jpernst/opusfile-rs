5758701406582710791fda5d65b4f91d67939dab
Jameson Ernst <j.patrick.ernst@gmail.com>
2015-09-29 23:04:45 -0700

    Docs 0.1.1

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['opusfile'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>StdError</a> for <a class='enum' href='opusfile/enum.OpusFileError.html' title='opusfile::OpusFileError'>OpusFileError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
